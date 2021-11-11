e87e01f05d1e92951be5fad247d6c5b96c667cc7
小建 <80093942+zed-2021@users.noreply.github.com>
2021-11-11 11:32:12 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,8 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("push test2");
+        System.out.println("push test2");
+        
 
 
 
